8a41de690f41ca62494e4555e89710aa71536639
emilyrobinson88 <robinson.2119@osu.edu>
2020-11-02 19:20:40 -0500

    modified the helper function to help valifate passwords

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -1,15 +1,42 @@
 module DictionariesHelper
 
 	def strength_analysis(dictionary)
-		if(dictionary.match? /\A[A-Z]*\z/)
-			#upper = true
-		elsif (dictionary.match? /\A[a-z]*\z/)
-			#lower = true
-		elsif (dictionary.match? /\A[0-9]*\z/)
-			#number = true
-		elsif (dictionary.match? /\A[!#$%&()*+,:;<=>?]*\z/)
-			#symbol = true
-			"True"
+
+		if(dictionary.count("A-Z") > 0)
+			upper = true
 		end
+		if (dictionary.count("a-z") > 0)
+			lower = true
+		end
+		if (dictionary.count("0-9") > 0)
+			number = true
+		end
+		if (dictionary.count("!#$%&()*+,:;<=>?") >0)
+			symbol = true
+			 
+			
+		end
+		if(upper==true and lower == true && number == true && symbol == true )
+			 return " This is a Strong Password"
+		else 
+			return "This is a weak password"
+		end
+
+	end
+
+	def strength_comp(dictionary)
+		up=dictionary.count("A-Z") *0.2
+		lo=dictionary.count("a-z") * 0.1
+		nu=dictionary.count("0-9") *0.3
+		sy=dictionary.count("!#$%&()*+,:;<=>?") *0.4
+		ttl=up+lo+nu+sy
+		return ttl
+		if(ttl>3 && dictionary.length>8)
+			return" This is a strong password"
+
+		else
+			return "this is a weak password"
+		end
+
 	end
 end
