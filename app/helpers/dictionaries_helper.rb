21eb9be2462d3d2f04d7c5bda6ecc22558b9b61e
emilyrobinson88 <robinson.2119@osu.edu>
2020-11-08 14:07:27 -0500

    updated helper messages and flow of program

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -30,13 +30,49 @@ module DictionariesHelper
 		nu=dictionary.count("0-9") *0.3
 		sy=dictionary.count("!#$%&()*+,:;<=>?") *0.4
 		ttl=up+lo+nu+sy
-		return ttl
-		if(ttl>3 && dictionary.length>8)
-			return" This is a strong password"
+		#strings for printing out
+		strong="This is a strong password\n"
+		leng="the lenhgth is good\n"
+		
+		if(ttl>2.5 && dictionary.length>8)
+			return strong 
 
 		else
 			return "this is a weak password"
 		end
 
 	end
+	def updatelen(dictionary)
+		
+		if(dictionary.length<8)
+			return "You need to make your password longer" 
+		end
+	end
+	def updateU(dictionary)
+		up=dictionary.count("A-Z")
+		if(up<1)
+			return "You need to have an Uppercase letter" 
+		end
+	end
+	def updateL(dictionary)
+		lo=dictionary.count("a-z")
+		if(lo<1)
+			return "You need to have an Lowercase letter letter" 
+		end
+	end
+
+	def updateN(dictionary)
+		nu=dictionary.count("0-9")
+		if(nu<1)
+			return "You need to have a number" 
+		end
+	end
+
+	def updateS(dictionary)
+		sy=dictionary.count("!#$%&()*+,:;<=>?")
+		if(sy<1)
+			return "You need to have a symbol" 
+		end
+	end
+
 end
